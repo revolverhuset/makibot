4dc5e40b9e52e5a4a256388af55264773bdf7f24
Jon Packer <contact@jonpacker.com>
2015-06-12 12:29:40 +0200

    list isushi menu pages

---
+++ fetch_price.js
@@ -0,0 +1,12 @@
+var menuPages = [
+  'http://www.isushi.no/maki',
+  'http://www.isushi.no/mix/',
+  'http://www.isushi.no/kombo-4/',
+  'http://www.isushi.no/maki-box/',
+  'http://www.isushi.no/nigiri/',
+  'http://www.isushi.no/sashimi/',
+  'http://www.isushi.no/smaretter/',
+  'http://www.isushi.no/ekstra/'
+];
+
+
