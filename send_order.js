cdf24def3257222a41300b7bfd280a579e1a1dfb
Jon Packer <contact@jonpacker.com>
2015-11-20 11:01:56 +0100

    add order sender

---
+++ send_order.js
@@ -0,0 +1,76 @@
+var async = require('async');
+var _ = require('underscore');
+var price = require('./fetch_price');
+var request = require('request');
+var serialize = require('serialize-like-php');
+
+var user = {
+  customer_id: '1146',
+  customer_name: 'Jon+Packer',
+  customer_mobile: '41191197',
+  customer_discount_percent: 0,
+  delivery_address: {
+    address: 'Olav+Kyrres+gate+28',
+    zip: '5015',
+    city: 'BERGEN',
+    entrance: '',
+    floor: '',
+    extra_address_info: ''
+  }
+};
+
+module.exports = function(order, callback) {
+  matchOrders(order, function(e, matched) {
+    if (e) return callback(e);
+    var postData = {
+      address: user.delivery_address.address.replace('+', ' '),
+      city: 'BERGEN',
+      confirm_delivery: 1,
+      delivery_days_from_now: 0,
+      delivery_time: 0,
+      entrance: '',
+      extra_address_info: 'Hentes utenfor bygget',
+      floor: '',
+      frontline_store_sync_code_that_covers_zip: 457041565642,
+      note: getBoxPartitioning(matched),
+      payment_method: 'bank_terminal',
+      zip: 5015
+    };
+    var cart = _.extend({ products: getCartObject(matched) }, user);
+    request.post('http://bestill.isushi.no/shop/checkout', {
+      form: postData,
+      followRedirect: false,
+      headers: {
+        Cookie: 'shopping_cart=' + encodeURIComponent(serialize.serialize(cart))
+      }
+    }, function(err, response, body) {
+      console.log(err, response, body);
+    });
+  })
+}
+
+function matchOrders(order, cb) {
+  async.map(order.orders, function(order, cb) {
+    price.fetchMatchesForOrder(order.text, function(e, matches) {
+      if (e) return cb(e);
+      else cb(null, {matches: matches, user:order.user});
+    });
+  }, cb)
+}
+
+function getCartObject(orders) {
+  return orders.reduce(function(cart, order) {
+    order.matches.forEach(function(match) {
+      if (cart[match.id] == null) cart[match.id] = 1;
+      else ++cart[match.id];
+    });
+    return cart;
+  }, {});
+}
+
+function getBoxPartitioning(orders) {
+  return orders.reduce(function(partitioning, order, i) {
+    partitioning.push('Box ' + (i+1) + ': ' + _.pluck(order.matches, 'name').join(', '));
+    return partitioning;
+  }, [orders.length + ' sett med pinner & soyasaus']).join(' \n');
+};
