acb49b1dd00b903f5491c7f97e5041aacc6a1dbf
Jon Packer <contact@jonpacker.com>
2016-05-04 14:27:35 +0200

    how was that ever working

--- makibot.js
+++ makibot.js
@@ -235,8 +235,7 @@ var handlers = {
 
       // Total is simply built as a string to preserve the fraction.
       Object.keys(accounts).map(function(key, idx) {
-        
-        accounts[key] = '' + accounts[key] + ' '+ (DELIVERY_COST * accountOccurances[alias])+'/'+order.orders.length;
+        accounts[key] = '' + accounts[key] + ' '+ (DELIVERY_COST * accountOccurances[key])+'/'+order.orders.length;
       });
 
       return sharebill(accounts, totalPrice, args, function(error, result) {
